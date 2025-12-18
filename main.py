b65e359d52566853207720bfb877e01d19502c58
xTrolek <xtrolekqminecraft@gmail.com>
2025-12-18 19:07:31 +0100

    main.py

--- main.py
+++ main.py
@@ -2,12 +2,12 @@ import discord
 from discord.ext import commands
 from bot_logic import gen_pass
 import random
+import os
 
 intents = discord.Intents.default()
 intents.message_content = True
 
 bot = commands.Bot(command_prefix='!', intents=intents)
-
 @bot.event
 async def on_ready():
     print(f'Zalogowaliśmy się jako {bot.user}')
@@ -40,4 +40,24 @@ async def joined(ctx, member: discord.Member):
         await ctx.send(f'{member} has no join date.')
     else:
         await ctx.send(f'{member} joined {discord.utils.format_dt(member.joined_at)}')
+@bot.command()
+async def losuj(ctx):
+    x = random.randint(1,100000)
+    await ctx.send(f"🎲 Wylosowana liczba: **{x}**")
+@bot.command()
+async def blackjack(ctx):
+    r = random.randint(1, 3)
+    if r==1:
+        wynik = ':red_square:'
+    elif r==2:
+        wynik = ':black_large_square:'
+    else:
+        wynik = ':green_square:'
+    await ctx.send(f"Wylosowałeś: {wynik}")
+@bot.command()
+async def ping(ctx):
+    await ctx.send(f"Pong! {round(bot.latency*1000)} ms")
+
+
+
 bot.run('TOKEN')
